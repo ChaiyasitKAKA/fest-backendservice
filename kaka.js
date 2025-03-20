4ebced3747bc013d14b1d62a2a716b1ddc68ec37
ChaiyasitKAKA <s6652410025@sau.ac.th>
2025-03-20 16:12:23 +0700

    fr cool asf

--- kaka.js
+++ kaka.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const userRoutes = require("./routes/userroutes");
+const festRoutes = require("./routes/festroutes");
 
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
-app.use('/user', userRoutes);
-
+app.use("/user", userRoutes);
+app.use("/fest", festRoutes);
+app.use("/images/fests", express.static);
 //test request/response
 app.get("/", (req, res) => {
-    res.json({
-        message: "Hello ,welcom to our server kakalamand"
-    });
-})
+  res.json({
+    message: "Hello ,welcom to our server kakalamand",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`); 
+  console.log(`Server is running on ${port}`);
 });
